c58ecc67f2d9049b05357df7af98228611e423ad
14042004-24032006 <poonguzhalic.dev@gmail.com>
2025-07-31 12:41:35 +0530

    Final commit

--- Design-Pattern/src/factory_pattern/PushNotification.java
+++ Design-Pattern/src/factory_pattern/PushNotification.java
@@ -3,7 +3,7 @@ package factory_pattern;
 public class PushNotification implements Notification{
 
 	@Override
-	public void sendNotification() {
+	public void sendNotification(String notification) {
 		 System.out.println("Notification via Push");
 		
 	}
